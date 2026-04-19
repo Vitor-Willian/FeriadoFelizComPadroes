77c0bc86bb645f5cf6df51e8cce2836b73e91ba7
Vitor-Willian <vitor.peneluppi@unifesp.br>
2026-04-18 22:21:14 -0300

    Criado a configuração global com o padrão Singleton

--- RPG_game/src/App.java
+++ RPG_game/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) {
+
+        Config config = Config.getInstance();
+        config.setDificuldade("Difícil");
+        config.setForcaMax(150);
+        config.carregarConfig();
+
         Personagem guerreiro = PersonagemFactory.criarGuerreiro("Aragorn", 50);
         Personagem arqueiro = PersonagemFactory.criarArqueiro("Legolas", 40);
         Personagem mago = PersonagemFactory.criarMago("Gandalf", 60);
@@ -8,5 +14,6 @@ public class App {
         
         guerreiro.ataque(goblin);
         arqueiro.ataque(goblin);
+
     }
 }
